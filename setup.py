5d5cc66887646ce3d579d82ee098011f12de19ad
mach1ne <luis_roque@live.com>
2021-11-19 17:47:15 +0000

    requirements and setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from os.path import dirname, join, realpath
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Robust time series augmentation for forecasting algorithms'
 LONG_DESCRIPTION = 'A package that allows to augment your time series dataset ' \
                    'to test the robustness of forecasting algorithms'
+
+PROJECT_ROOT = dirname(realpath(__file__))
+REQUIREMENTS_FILE = join(PROJECT_ROOT, "requirements.txt")
+with open(REQUIREMENTS_FILE) as f:
+    install_reqs = f.read().splitlines()
 
 # Setting up
 setup(
@@ -15,14 +21,13 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=install_reqs,
     keywords=['python', 'time series', 'hierarchical', 'forecasting', 'augmentation', 'machine learning'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Researchers",
+        "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
     ]
 )
